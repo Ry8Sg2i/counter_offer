e7d29fbe99960fce48392e23032f9063e5df6055
Ryu <azatose8684@gmail.com>
2023-10-31 15:57:40 +0900

    bottomnavigaterbar

---
+++ lib/src/app.dart
@@ -0,0 +1,43 @@
+import 'package:flutter/material.dart';
+import 'package:counterofferv1/src/screan/register/chat_page.dart';
+import 'package:counterofferv1/src/screan/my_page.dart';
+import 'package:counterofferv1/src/screan/setting_page.dart';
+
+class MyStatefulWidget extends StatefulWidget {
+  const MyStatefulWidget({Key? key}) : super(key: key);
+
+  @override
+  State<MyStatefulWidget> createState() => _MyStatefulWidgetState();
+}
+
+class _MyStatefulWidgetState extends State<MyStatefulWidget> {
+  static const _screens = [
+    ChatPage(),
+    MyPage(),
+    SettingPage(),
+  ];
+
+  int _selectedIndex = 0;
+
+  void _onItemTapped(int index) {
+    setState(() {
+      _selectedIndex = index;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        body: _screens[_selectedIndex],
+        bottomNavigationBar: BottomNavigationBar(
+          currentIndex: _selectedIndex,
+          onTap: _onItemTapped,
+          items: const <BottomNavigationBarItem>[
+            BottomNavigationBarItem(icon: Icon(Icons.home), label: 'Home'),
+            BottomNavigationBarItem(icon: Icon(Icons.account_box), label: 'Account'),
+            BottomNavigationBarItem(icon: Icon(Icons.settings), label: 'Setting'),
+          ],
+          type: BottomNavigationBarType.fixed,
+        ));
+  }
+}
