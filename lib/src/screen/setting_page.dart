21410859a448be21f4daf5c9ff00ae52a2c7c271
Ryu <azatose8684@gmail.com>
2023-10-31 15:56:59 +0900

    設定画面

---
+++ lib/src/screen/setting_page.dart
@@ -0,0 +1,34 @@
+import 'package:flutter/material.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:counterofferv1/auth/login_page.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+
+// ConsumerWidgetでProviderから値を受け渡す
+class SettingPage extends ConsumerWidget {
+  const SettingPage({super.key});
+
+  @override
+  Widget build(BuildContext context, WidgetRef ref) {
+    // Providerから値を受け取る
+
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Setting'),
+        actions: <Widget>[
+          IconButton(
+            icon: const Icon(Icons.logout),
+            onPressed: () async {
+              FirebaseAuth.instance.signOut();
+              await Navigator.of(context).pushAndRemoveUntil(
+                MaterialPageRoute(
+                  builder: (context) => const LoginPage(),
+                ),
+                    (route) => false,
+              );
+            },
+          ),
+        ],
+      ),
+    );
+  }
+}
