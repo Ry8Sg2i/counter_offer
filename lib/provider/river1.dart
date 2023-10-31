3317001c77f29070595a9e641183fdbe9290836a
Ryu <azatose8684@gmail.com>
2023-10-31 15:44:49 +0900

    Providerの追加

---
+++ lib/provider/river1.dart
@@ -0,0 +1,50 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+
+// ユーザー情報の受け渡しを行うためのProvider
+final userProvider = StateProvider.autoDispose((ref) {
+  return FirebaseAuth.instance.currentUser;
+});
+
+final uidProvider = StateProvider.autoDispose((ref) {
+  return FirebaseAuth.instance.currentUser?.uid;
+});
+
+// エラー情報の受け渡しを行うためのProvider
+final infoTextProvider = StateProvider.autoDispose((ref) {
+  return '';
+});
+
+// メールアドレスの受け渡しを行うためのProvider
+final emailProvider = StateProvider.autoDispose((ref) {
+  return '';
+});
+
+// パスワードの受け渡しを行うためのProvider
+final passwordProvider = StateProvider.autoDispose((ref) {
+  return '';
+});
+
+// 名前の受け渡しを行うためのProvider
+final userNameProvider = StateProvider.autoDispose((ref) {
+  return '';
+});
+
+// emailの受け渡しを行うためのProvider
+final userEmailProvider = StateProvider.autoDispose((ref) {
+  return '';
+});
+
+// githubidの受け渡しを行うためのProvider
+final userGitProvider = StateProvider.autoDispose((ref) {
+  return '';
+});
+
+// StreamProviderを使うことでStreamも扱うことができる
+final postsQueryProvider = StreamProvider.autoDispose((ref) {
+  return FirebaseFirestore.instance
+      .collection('posts')
+      .orderBy('date')
+      .snapshots();
+});
