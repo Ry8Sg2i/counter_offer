18ddc08e02f2cb231e47c4f2bfe74179a747c71e
Ryu <azatose8684@gmail.com>
2023-11-01 23:15:00 +0900

    path変更

--- lib/src/screen/register/chat_page.dart
+++ lib/src/screen/register/chat_page.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
-import 'package:counterofferv1/src/screan/register/addPost_page.dart';
+import 'package:counterofferv1/src/screen/register/addPost_page.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:counterofferv1/provider/river1.dart';
 
