d82e665ba44ebe64cfaf11e3f9de7e5524d390ac
Ryu <azatose8684@gmail.com>
2023-11-05 22:27:12 +0900

    appbarの編集

--- lib/src/screen/setting_page.dart
+++ lib/src/screen/setting_page.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:counterofferv1/auth/login_page.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:counterofferv1/provider/river1.dart';
 
 // ConsumerWidgetでProviderから値を受け渡す
 class SettingPage extends ConsumerWidget {
@@ -10,28 +11,53 @@ class SettingPage extends ConsumerWidget {
   @override
   Widget build(BuildContext context, WidgetRef ref) {
     // Providerから値を受け取る
+    final User user = ref.watch(userProvider.notifier).state!;
 
     return Scaffold(
       appBar: AppBar(
-        title: const Text('Setting'),
-        titleTextStyle: const TextStyle(
-          fontSize: 20,
-          color: Colors.greenAccent
+        title: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            const Text(
+                "Setting",
+                style: TextStyle(color: Colors.greenAccent, fontSize: 20),
+              ),
+            Text(
+                '${user.email}',
+                style: const TextStyle(color: Colors.white, fontSize: 14.0),
+              ),
+            ]
           ),
-        actions: <Widget>[
-          IconButton(
-            icon: const Icon(Icons.logout),
-            onPressed: () async {
-              FirebaseAuth.instance.signOut();
-              await Navigator.of(context).pushAndRemoveUntil(
-                MaterialPageRoute(
-                  builder: (context) => const LoginPage(),
-                ),
-                    (route) => false,
-              );
-            },
-          ),
-        ],
+      ),
+      body: Center(
+        child: Column(
+          children: [
+            Card(
+              child: ExpansionTile(
+                title: const Text("logout"),
+                children: <Widget>[
+                  TextButton(
+                    onPressed: () async {
+                      FirebaseAuth.instance.signOut();
+                      await Navigator.of(context).pushAndRemoveUntil(
+                        MaterialPageRoute(
+                          builder: (context) => const LoginPage(),
+                          ),
+                        (route) => false);
+                    }, 
+                    child: const Text(
+                      'Logout',
+                      style: TextStyle(
+                        color: Colors.red
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          ],
+        ),
       ),
     );
   }
