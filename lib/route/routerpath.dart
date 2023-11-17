528aec785b331feb350dfce1a8ad795a28497757
Ryu <azatose8684@gmail.com>
2023-11-17 20:25:55 +0900

    試作

---
+++ lib/route/routerpath.dart
@@ -0,0 +1,17 @@
+// // 直接パスを書かなくていいように、パスを定数として定義しておきます。
+// class RouterPath {
+//   // トップレベルのパスは、 / を付けます。
+//   static const String home = '/';
+//   static const String navbar = '/navbar';
+//   static const String main = '/main';
+//   static const String account = '/account';
+//   static const String addaccount = 'addaccount';
+//   static const String acountadd = '/account/addaccount';
+//   static const String setting = '/Setting';
+//   static const String accountSetting = 'accountsetting';// ネストしたパスは / を付けない
+//   static const String settingaccount = '/Setting/accountsetting';// context.goで使うパス
+//   static const String logoutSetting = 'logoutsetting';
+//   static const String settinglogout = '/Setting/accountsetting/logoutsetting';
+//   static const String deleteaccountSetting = 'deleteaccount';
+//   static const String settingdeleteaccount = '/Setting/accountsetting/deleteaccount';
+// }
