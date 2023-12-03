44bda3b0de13da27582d1879121dbb22eb3f15db
Ryu <azatose8684@gmail.com>
2023-12-04 00:00:00 +0900

    試作

--- lib/src/screen/setting/setting_page.dart
+++ lib/src/screen/setting/setting_page.dart
@@ -1,11 +1,9 @@
 // ignore: unused_import
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
-import 'package:firebase_auth/firebase_auth.dart';
 // ignore: unused_import
 import 'package:counterofferv1/auth/login_page.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
-import 'package:counterofferv1/provider/river1.dart';
 import 'package:go_router/go_router.dart';
 
 // ConsumerWidgetでProviderから値を受け渡す
@@ -15,21 +13,16 @@ class SettingPage extends ConsumerWidget {
   @override
   Widget build(BuildContext context, WidgetRef ref) {
     // Providerから値を受け取る
-    final User user = ref.watch(userProvider.notifier).state!;
 
     return Scaffold(
       appBar: AppBar(
-        title: Column(
+        title: const Column(
           mainAxisAlignment: MainAxisAlignment.center,
           crossAxisAlignment: CrossAxisAlignment.center,
           children: [
-            const Text(
+            Text(
                 "Setting",
                 style: TextStyle(color: Colors.greenAccent, fontSize: 20),
-              ),
-            Text(
-                '${user.email}',
-                style: const TextStyle(color: Colors.white, fontSize: 14.0),
               ),
             ]
           ),
