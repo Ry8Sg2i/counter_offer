1ac369ae551e1b323b57376a2adf0de23bfb2130
Ryu <azatose8684@gmail.com>
2023-12-06 22:58:42 +0900

    mypage流用

--- lib/src/screen/register/anotheruser.dart
+++ lib/src/screen/register/anotheruser.dart
@@ -1,10 +1,20 @@
 import 'package:flutter/material.dart';
+import 'package:counterofferv1/model/user1.dart';
+import 'package:counterofferv1/provider/river1.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 
-class AnotherUser extends StatelessWidget {
-  const AnotherUser({super.key});
+class AnotherUser extends ConsumerWidget {
+  const AnotherUser({
+    super.key,
+      required this.user1Data
+  });
+  final User1 user1Data;
 
   @override
-  Widget build(BuildContext context) {
+  Widget build(BuildContext context, WidgetRef ref) {
+
+    final user1Data = ref.watch(user1Provider);
+
     return Scaffold(
       appBar: AppBar(
         title: const Column(
@@ -17,6 +27,32 @@ class AnotherUser extends StatelessWidget {
             ),
           ]
         ),
+      ),
+      body: user1Data.when(data: (data) {
+        return  Card(
+          child: ExpansionTile(
+            leading: const Icon(Icons.description),
+            title: const Text("name"),
+            children: <Widget>[
+              ListTile(
+                title: Text(data.name),
+              ),
+            ],
+          ),
+        );
+      },
+        // 値が読込中のとき
+        loading: () {
+          return const Center(
+            child: Text('読込中...'),
+          );
+        },
+        // 値の取得に失敗したとき
+        error: (e, stackTrace) {
+          return Center(
+            child: Text(e.toString()),
+          );
+        },
       ),
     );
   }
