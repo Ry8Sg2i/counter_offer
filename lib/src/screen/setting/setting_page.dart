ce1093f2efafe9e10e4d70a306ee3ecc3030bc40
Ryu <azatose8684@gmail.com>
2023-12-12 16:40:55 +0900

    appbar変更

--- lib/src/screen/setting/setting_page.dart
+++ lib/src/screen/setting/setting_page.dart
@@ -1,5 +1,6 @@
 // ignore: unused_import
 import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:counterofferv1/compornents/appbar.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:go_router/go_router.dart';
@@ -13,25 +14,8 @@ class SettingPage extends ConsumerWidget {
     // Providerから値を受け取る
 
     return Scaffold(
-      appBar: AppBar(
-        title: const Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          crossAxisAlignment: CrossAxisAlignment.center,
-          children: [
-            Text(
-                "Setting",
-                style: TextStyle(color: Colors.greenAccent, fontSize: 20),
-              ),
-            ]
-          ),
-          bottom: PreferredSize(
-          preferredSize: const Size.fromHeight(5),
-          child: Container(
-            height: 10,
-            color: Colors.white,
-          ),
-        ),
-        backgroundColor: Colors.black,
+      appBar: const AppBarComponentWidget(
+        title: "Setting",
       ),
       body: Center(
         child: Column(
