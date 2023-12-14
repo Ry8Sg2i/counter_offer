56a8e461ba0322861f893891524b4f3993039d45
Ryu <azatose8684@gmail.com>
2023-12-14 16:31:52 +0900

    appbarデザインText変更

--- lib/src/screen/setting/accoumt/logout_page.dart
+++ lib/src/screen/setting/accoumt/logout_page.dart
@@ -1,36 +1,22 @@
 // ignore_for_file: camel_case_types
+import 'package:counterofferv1/compornents/appbar.dart';
 import 'package:flutter/material.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
-import 'package:counterofferv1/provider/river1.dart';
 import 'package:go_router/go_router.dart';
+import 'package:counterofferv1/provider/river1.dart';
 
 class logoutSetting extends ConsumerWidget {
   const logoutSetting({super.key});
 
   @override
   Widget build(BuildContext context, WidgetRef ref) {
-    // Providerから値を受け取る
+
     final User user = ref.watch(userProvider.notifier).state!;
 
     return Scaffold(
-      appBar: AppBar(
-        title: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          crossAxisAlignment: CrossAxisAlignment.center,
-          children: [
-            const Text(
-                "Logout",
-                style: TextStyle(color: Colors.greenAccent, fontSize: 20),
-              ),
-            Text(
-                '${user.email}',
-                style: const TextStyle(color: Colors.white, fontSize: 14.0),
-              ),
-            ]
-          ),
-        backgroundColor: Colors.black,
-        iconTheme: const IconThemeData(color: Colors.white),
+      appBar: const AppBarComponentWidget(
+        title: "Setting",
       ),
       body: Center(
         child: Column(
@@ -39,7 +25,7 @@ class logoutSetting extends ConsumerWidget {
               children: [
                 Container(
                   margin: const EdgeInsets.only(top: 10,bottom: 10,right: 10,left: 10,),
-                  child: const Text("アカウントからログアウトします。"),
+                  child: Text("現在のアカウント${user.email}\nアカウントからログアウトします。"),
                 ),
               ],
             ),
